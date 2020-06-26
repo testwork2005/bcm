6fd85320f397e639f76b56cf06fb3a5404adacc5
blondycage <yakson500@gmail.com>
2020-06-25 22:29:40 -0700

    build

---
+++ build/precache-manifest.1fb9a1600ec96f2d6114f2ec825dfc42.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "81bc3e622c1d3c9b3c7eb1b13c3ae892",
+    "url": "/static/media/Mining.81bc3e62.gif"
+  },
+  {
+    "revision": "85a54cc174c1878903d5",
+    "url": "/static/css/main.b8102124.chunk.css"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ef61f0e42f099ac13ccd",
+    "url": "/static/js/1.ef61f0e4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba74583974466b954dd54ffbae425f31",
+    "url": "/static/media/home-bg.ba745839.jpg"
+  },
+  {
+    "revision": "85a54cc174c1878903d5",
+    "url": "/static/js/main.85a54cc1.chunk.js"
+  },
+  {
+    "revision": "23897fb03ef69488babd2a3b6a7ca4db",
+    "url": "/static/media/bc.23897fb0.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ef61f0e42f099ac13ccd",
+    "url": "/static/css/1.4ac289e6.chunk.css"
+  },
+  {
+    "revision": "78acc80e1511c8ba91b628e35a22db96",
+    "url": "/index.html"
+  }
+];
